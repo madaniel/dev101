ded639e928443200cd63f78eaa9c5427c23c82eb
Daniel Madar <m.a.daniel@gmail.com>
2021-08-14 23:35:35 +0300

    added FibCounter

--- examples/iter.py
+++ examples/iter.py
@@ -3,13 +3,14 @@ class Counter(object):
 
     def __init__(self):
         self.current = 0
-        self.limit = 10
+        self.stop = 10
 
     def __iter__(self):
+        self.__init__()
         return self
 
     def __next__(self):
-        if self.current < self.limit:
+        if self.current < self.stop:
             self.current += 1
         else:
             raise StopIteration
@@ -25,6 +26,7 @@ class CounterRange(object):
         self.step = step
 
     def __iter__(self):
+        self.__init__()
         return self
 
     def __next__(self):
@@ -36,6 +38,39 @@ class CounterRange(object):
         return self.current
 
 
+class FibCounter(object):
+
+    def __init__(self, stop: int = 200):
+        self.counter = 0
+        self.current = 1
+        self.stop = stop
+        self.current_minus_1 = 1
+        self.current_minus_2 = 1
+
+    def __iter__(self):
+        self.__init__()
+        return self
+
+    def __next__(self):
+        start_seq_value = {0: 0,
+                           1: 1,
+                           2: 1
+                           }.get(self.counter)
+
+        if start_seq_value is not None:
+            self.counter += 1
+            return start_seq_value
+
+        self.current_minus_2 = self.current_minus_1
+        self.current_minus_1 = self.current
+        self.current = self.current_minus_2 + self.current_minus_1
+
+        if self.current <= self.stop:
+            return self.current
+
+        raise StopIteration
+
+
 if __name__ == '__main__':
     c = Counter()
 
@@ -45,4 +80,9 @@ if __name__ == '__main__':
     cr = CounterRange(start=10, stop=0, step=-1)
 
     for number in cr:
+        print(number)
+
+    f = FibCounter()
+
+    for number in f:
         print(number)
